152784130caf5bb0fb26619dc8597bbda25f5f2a
Yourmery Haddad <yourmeryhaddad@gmail.com>
2021-04-18 21:28:15 -0400

    Update structure for routes

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const http = require('http')
 const app = express()
 
-const product_router = require('./api_products');
+// Routes
+const product_router = require('./api/api_products');
+const category_router = require('./api/api_categories');
 
 app.use('/api/products/', product_router);
-
-const category_router = require('./api_categories');
-
 app.use('/api/categories/', category_router);
+
+
 
 http.createServer(app).listen(8001, () => {
   console.log('Server started at http://localhost:8001');
