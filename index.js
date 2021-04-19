e3f2a8bed9fccd200db9738c4a2c6150b7838af0
Yourmery Haddad <yourmeryhaddad@gmail.com>
2021-04-18 22:39:14 -0400

    Fix typo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// import cors from 'cors';
-
-const app = express()
+import cors from 'cors';
 const express = require('express')
 const http = require('http')
+
+const app = express()
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +11,7 @@ const product_router = require('./api/api_products');
 const category_router = require('./api/api_categories');
 
 // Add Access Control Allow Origin headers
-// app.use(cors());
+app.use(cors());
 
 app.use('/api/products/', product_router);
 app.use('/api/categories/', category_router);
