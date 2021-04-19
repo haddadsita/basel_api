ea1d46a1725919f617e05e302a6207d411f5c20e
Yourmery Haddad <yourmeryhaddad@gmail.com>
2021-04-18 22:32:13 -0400

    Import and enabled cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import cors from 'cors';
+
+const app = express()
 const express = require('express')
 const http = require('http')
-const app = express()
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,14 +11,7 @@ const product_router = require('./api/api_products');
 const category_router = require('./api/api_categories');
 
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://bsale-frontend.herokuapp.com/");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.use('/api/products/', product_router);
 app.use('/api/categories/', category_router);
