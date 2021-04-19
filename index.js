78d509e999b05948f75543ff052e5f2e1c89d444
Yourmery Haddad <yourmeryhaddad@gmail.com>
2021-04-18 22:05:17 -0400

    Update index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const http = require('http')
 const app = express()
 
+const PORT = process.env.PORT || 3000;
+
 // Routes
 const product_router = require('./api/api_products');
 const category_router = require('./api/api_categories');
@@ -11,6 +13,6 @@ app.use('/api/categories/', category_router);
 
 
 
-http.createServer(app).listen(8001, () => {
-  console.log('Server started at http://localhost:8001');
+http.createServer(app).listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
